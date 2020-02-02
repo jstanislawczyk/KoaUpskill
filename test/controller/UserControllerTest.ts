07b2e0466d5275d716c9fd66aac94dfcef1d6c16
stanislawczykj <jakubstanislawczyk98@gmail.com>
2020-02-02 21:31:52 +0100

    Example user tests

--- test/controller/UserControllerTest.ts
+++ test/controller/UserControllerTest.ts
@@ -1,14 +1,50 @@
-import { expect } from 'chai';
 import { User } from '../../src/entity/User';
-import request = require('request');
 import 'mocha'; 
 import 'reflect-metadata';
 
+const serverAddress = 'http://localhost:3000';
+const usersRequestPath = '/api/users';
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
 describe('Users', () => {
-    it('Main page content', function(done) {
-        request('http://localhost:8080/api/users' , function(error: any, response: any, body: any) {
-            expect(response.statusCode).to.equal(200);
-            done();
-        });
+    it('Saves users', function(done) {
+        const newUserForTest = {
+            name: 'TEST_USER',
+            age: 22,
+        };
+
+        chai.request(serverAddress)
+            .post(usersRequestPath)
+            .set('content-type', 'application/json')
+            .send(newUserForTest)
+            .end((err: any, res: any) => {
+                expect(res).to.have.status(201);
+                done();
+            });
+    });
+
+    it('Returns all users', function(done) {
+        const newUserForTest = {
+            name: 'TEST_USER',
+            age: 22,
+        };
+
+        chai.request(serverAddress)
+            .get(usersRequestPath)
+            .end((err: any, res: any) => {
+                expect(res).to.have.status(200);
+                expect(res.body)
+                    .to.be.an.instanceof(Array)
+                    .and.to.have.property(0)
+                    .that.includes.all.keys([ 'id', 'name', 'age' ])
+                    .to.include(newUserForTest);
+
+                done();
+            });
     });
 })
