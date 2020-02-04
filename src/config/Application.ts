f4cb4cd083f89ca8025bfe0c27180d1630f45470
stanislawczykj <jakubstanislawczyk98@gmail.com>
2020-02-04 22:10:09 +0100

    Dynamic node config inlcuded

--- src/config/Application.ts
+++ src/config/Application.ts
@@ -1,16 +1,16 @@
 import 'reflect-metadata';
 import { createKoaServer, useContainer } from 'routing-controllers';
-import { createConnection, useContainer as useTypeOrmContainer, Connection, ConnectionOptions } from 'typeorm';
+import { createConnection, useContainer as useTypeOrmContainer, Connection } from 'typeorm';
 import { Container } from 'typedi';
-import * as koaLogger from 'koa-logger';
 import { DatabaseConfig } from '../config/DatabaseConfig';
+import * as koaLogger from 'koa-logger';
+import * as config from 'config';
 
 export class Application {
     public databaseConnection: Connection;
-    public app: any;
 
     public async start(): Promise<void> {
-        const databaseConfig = DatabaseConfig.databaseConnectionConfiguration;
+        const databaseConfig = DatabaseConfig.getDatabaseConnectionConfiguration();
 
         useContainer(Container);
         useTypeOrmContainer(Container);
@@ -18,17 +18,17 @@ export class Application {
         await createConnection(databaseConfig)
             .then(async connection => {
                 this.databaseConnection = connection;
-                const appName = require('./../../package').name;
-                const port = 3000;
 
-                this.app = createKoaServer({
+                const port = config.get('app.port');
+                const appName = config.get('app.name');
+                const app = createKoaServer({
                     controllers: [__dirname + '/../controller/*.ts'],
                     routePrefix: '/api',
                 });
 
-                this.app.use(koaLogger());
-                this.app.address = () => 'http://localhost:3000';
-                this.app.listen(port, () => {
+                app.use(koaLogger());
+
+                app.listen(port, () => {
                     console.log(`${appName} server runs on port ${port}`)
                 });
             })
--- src/config/DatabaseConfig.ts
+++ src/config/DatabaseConfig.ts
@@ -1,29 +1,37 @@
 import { ConnectionOptions } from 'typeorm';
+import * as config from 'config';
 
 export class DatabaseConfig {
 
-    public static databaseConnectionConfiguration: ConnectionOptions = {
-        type: 'mongodb',
-        host: 'localhost',
-        database: 'koajs',
-        port: 27017,
-        dropSchema: true,
-        synchronize: true,
-        logging: true,
-        entities: [
-            'src/entity/**/*.ts',
-        ],
-        migrations: [
-           'src/migration/**/*.ts',
-        ],
-        subscribers: [
-           'src/subscriber/**/*.ts',
-        ],
-        cli: {
-           entitiesDir: 'src/entity',
-           migrationsDir: 'src/migration',
-           subscribersDir: 'src/subscriber',
-        },
-        useUnifiedTopology: true,
+    public static getDatabaseConnectionConfiguration(): ConnectionOptions {
+        const host = config.get('db.host');
+        const port = config.get('db.port');
+        const name = config.get('db.name');
+        const dropSchema = config.get('db.dropSchema');
+
+        return <ConnectionOptions> {
+            type: 'mongodb',
+            host: host,
+            port: port,
+            database: name,
+            dropSchema: dropSchema,
+            synchronize: true,
+            logging: true,
+            entities: [
+                'src/entity/**/*.ts',
+            ],
+            migrations: [
+            'src/migration/**/*.ts',
+            ],
+            subscribers: [
+            'src/subscriber/**/*.ts',
+            ],
+            cli: {
+            entitiesDir: 'src/entity',
+            migrationsDir: 'src/migration',
+            subscribersDir: 'src/subscriber',
+            },
+            useUnifiedTopology: true,
+        }
     }
 }
