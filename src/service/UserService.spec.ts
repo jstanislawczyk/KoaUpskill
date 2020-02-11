6e2a45ffa91a806810099003e2cd7ce8e910481c
stanislawczykj <jakubstanislawczyk98@gmail.com>
2020-02-11 21:45:31 +0100

    Updated tests for user

--- src/service/UserService.spec.ts
+++ src/service/UserService.spec.ts
@@ -6,46 +6,54 @@ import { User } from '../entity/User';
 import { UserRole } from '../enum/UserRole';
 
 describe('Users service', () => {
-    it('getAllUsers()', async () => {
-        const userRepository: UserRepository = new UserRepository();
-        const userService: UserService = new UserService(userRepository);
+    describe('getAllUsers()', () => {
+        it('Should return all users', async () => {
+            const userRepository: UserRepository = new UserRepository();
+            const userService: UserService = new UserService(userRepository);
 
-        sinon.stub(userRepository, 'find' as any).resolves(getMultipleUsers());
+            sinon.stub(userRepository, 'find' as any).resolves(getMultipleUsers());
 
-        assert.deepEqual(await userService.getAllUsers(), getMultipleUsers());
+            assert.deepEqual(await userService.getAllUsers(), getMultipleUsers());
+        });
     });
 
-    it('getAllUsers() empty', async () => {
-        const userRepository: UserRepository = new UserRepository();
-        const userService: UserService = new UserService(userRepository);
+    describe('getAllUsers() empty', () => {
+        it('Should get empty users list', async () => {
+            const userRepository: UserRepository = new UserRepository();
+            const userService: UserService = new UserService(userRepository);
 
-        sinon.stub(userRepository, 'find' as any).resolves([]);
+            sinon.stub(userRepository, 'find' as any).resolves([]);
 
-        assert.deepEqual(await userService.getAllUsers(), []);
+            assert.deepEqual(await userService.getAllUsers(), []);
+        });
     });
 
-    it('findOneUser()', async () => {
-        const userRepository: UserRepository = new UserRepository();
-        const userService: UserService = new UserService(userRepository);
+    describe('findOneUser()', () => {
+        it('Should find one user', async () => {
+            const userRepository: UserRepository = new UserRepository();
+            const userService: UserService = new UserService(userRepository);
 
-        sinon.stub(userRepository, 'findOne' as any).resolves(getUser());
+            sinon.stub(userRepository, 'findOne' as any).resolves(getUser());
 
-        assert.deepEqual(await userService.findOneUser('Som3ID'), getUser());
+            assert.deepEqual(await userService.findOneUser('Som3ID'), getUser());
+        });
     });
 
-    it('updateUser()', async () => {
-        const userRepository: UserRepository = new UserRepository();
-        const userService: UserService = new UserService(userRepository);
+    describe('updateUser()', () => {
+        it('Should update user', async () => {
+            const userRepository: UserRepository = new UserRepository();
+            const userService: UserService = new UserService(userRepository);
 
-        const updatedUser: User = getUser();
-        updatedUser.firstName = 'UpdatedFirstName';
-        updatedUser.lastName = 'UpdatedLastName';
-        updatedUser.role = UserRole.MANAGER;
+            const updatedUser: User = getUser();
+            updatedUser.firstName = 'UpdatedFirstName';
+            updatedUser.lastName = 'UpdatedLastName';
+            updatedUser.role = UserRole.MANAGER;
 
-        sinon.stub(userRepository, 'findOne' as any).resolves(getUser());
-        sinon.stub(userRepository, 'save' as any).resolves(updatedUser);
+            sinon.stub(userRepository, 'findOne' as any).resolves(getUser());
+            sinon.stub(userRepository, 'save' as any).resolves(updatedUser);
 
-        assert.deepEqual(await userService.saveUser(updatedUser), updatedUser);
+            assert.deepEqual(await userService.saveUser(updatedUser), updatedUser);
+        });
     });
 });
 
