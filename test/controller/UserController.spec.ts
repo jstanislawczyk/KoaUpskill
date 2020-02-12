f9ec364e43228b5a8f2566b1b88ea3827cff00b5
jstanislawczyk <jstanislawczyk@pgs-soft.com>
2020-02-12 15:36:30 +0100

    Updated supertest for user

--- test/controller/UserController.spec.ts
+++ test/controller/UserController.spec.ts
@@ -1,18 +1,60 @@
-const request = require('supertest');
 import { Application } from '../../src/config/Application';
+import { assert } from 'chai';
+import * as request from 'supertest';
+import { getRepository } from 'typeorm';
+import { UserRole } from '../../src/enum/UserRole';
+import { User } from '../../src/entity/User';
+import { Logger } from '../../src/config/Logger';
+import { LoggerLevel } from '../../src/enum/LoggerLevel';
+import { UserDtoConverter } from '../../src/dto-converter/UserDtoConverter';
 
 const application: Application = new Application();
-const applicationStartContext = application.start()
+const applicationStartContext = application.start();
 
-describe('GET /api/users', () => {
-    it('respond with json containing a list of all users', async (done: Mocha.Done) => {
-        await applicationStartContext;
+describe('Users controller integration test', () => {
+    beforeEach(async () => {
+        return await application.databaseConnection
+            .synchronize(true)
+            .catch(error => 
+                Logger.log(`Test database error: ${error}`, LoggerLevel.ERROR)
+            );
+    });
 
-        request(application.appContext)
-            .get('/api/users')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
+    describe('GET /api/users', () => {
+        it('respond with json containing an empty list of users', async () => {
+            await applicationStartContext;
+           
+            return request(application.appContext)
+                .get('/api/users')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then((response: any) => {
+                    assert.deepEqual(response.body, []);
+                })
+        });
+    });
+
+    describe('GET /api/users', () => {
+        it('respond with json containing list of users', async () => {
+            await applicationStartContext;
+
+            const user: User = new User();
+            user.firstName = 'TT1';
+            user.lastName = 'TT2';
+            user.role = UserRole.ADMIN;
+            
+            await getRepository(User).save(user);
+            
+            return request(application.appContext)
+                .get('/api/users')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then((response: any) => {
+                    assert.deepEqual(response.body, [UserDtoConverter.toDto(user)]);
+                })
+        });
     });
 });
 
