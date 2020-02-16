05d82460134e342e198cf3e419f20f1490bc96f2
stanislawczykj <jakubstanislawczyk98@gmail.com>
2020-02-16 21:59:03 +0100

    Supplierr-invoice controller

---
+++ src/controller/SupplierInvoiceController.ts
@@ -0,0 +1,29 @@
+
+import { InvoiceService } from '../service/InvoiceService';
+import { JsonController, Get, NotFoundError, Param } from 'routing-controllers';
+import { InvoiceDtoConverter } from '../dto-converter/InvoiceDtoConverter';
+import { InvoiceDto } from '../dto/InvoiceDto';
+import { SupplierService } from '../service/SupplierService';
+
+@JsonController('/suppliers')
+export class InvoiceController {
+
+    constructor(
+        private readonly invoiceService: InvoiceService,
+        private readonly supplierService: SupplierService,
+    ) {
+    }
+
+    @Get('/:supplierId/invoices')
+    async getAllSupplierInvoices(@Param('supplierId') supplierId: string): Promise<InvoiceDto[]> {
+        await this.supplierService.findOneSupplier(supplierId)
+            .catch(() => {
+                throw new NotFoundError(`Supplier with id=${supplierId} not found`);
+            });
+
+        return await this.invoiceService.getAllSupplierInvoices(supplierId)
+            .then(invoices => 
+                InvoiceDtoConverter.toListOfDtos(invoices)    
+            );
+    }
+}
