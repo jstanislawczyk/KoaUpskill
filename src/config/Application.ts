3e6863ba61403334223c72c0876f42e8383a0970
jstanislawczyk <jstanislawczyk@pgs-soft.com>
2020-02-20 10:29:04 +0100

    Fixed indent in Application

--- src/config/Application.ts
+++ src/config/Application.ts
@@ -8,43 +8,43 @@ import { Logger } from './Logger';
 import { LoggerLevel } from '../enum/LoggerLevel';
 
 export class Application {
-    public databaseConnection: Connection;
-    public appContext: any;
+  public databaseConnection: Connection;
+  public appContext: any;
 
-    public async start(): Promise<void> {
-        const databaseConfig = DatabaseConfig.getDatabaseConnectionConfiguration();
+  public async start(): Promise<void> {
+    const databaseConfig = DatabaseConfig.getDatabaseConnectionConfiguration();
 
-        useContainer(Container);
-        useTypeOrmContainer(Container);
+    useContainer(Container);
+    useTypeOrmContainer(Container);
 
-        await createConnection(databaseConfig)
-            .then(async connection => {
-                this.databaseConnection = connection;
+    await createConnection(databaseConfig)
+      .then(async connection => {
+        this.databaseConnection = connection;
 
-                const port = config.get('app.port');
-                const appName = config.get('app.name');
-                const app = createKoaServer({
-                    controllers: [__dirname + '/../controller/*.ts'],
-                    middlewares: [__dirname + '/../middleware/*.ts'],
-                    routePrefix: '/api',
-                    defaultErrorHandler: false,
-                });
-
-                this.appContext = app.listen(port, () => {
-                    Logger.log(`${appName} server runs on port ${port}`);
-                });
-            })
-            .catch(error => 
-                Logger.log(`TypeORM connection error: ${error}`, LoggerLevel.ERROR)
-            );
-    }
-
-    public close(): void {
+        const port = config.get('app.port');
         const appName = config.get('app.name');
+        const app = createKoaServer({
+          controllers: [__dirname + '/../controller/*.ts'],
+          middlewares: [__dirname + '/../middleware/*.ts'],
+          routePrefix: '/api',
+          defaultErrorHandler: false,
+        });
 
-        this.databaseConnection.close().then(() => {});
-        this.appContext.close();
+        this.appContext = app.listen(port, () => {
+          Logger.log(`${appName} server runs on port ${port}`);
+        });
+      })
+      .catch(error =>
+        Logger.log(`TypeORM connection error: ${error}`, LoggerLevel.ERROR)
+      );
+  }
 
-        Logger.log(`${appName} server stopped`);
-    }
+  public async close(): Promise<void> {
+    const appName = config.get('app.name');
+
+    await this.databaseConnection.close();
+    await this.appContext.close();
+
+    Logger.log(`${appName} server stopped`);
+  }
 }
